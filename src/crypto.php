c09b6db2650ba8e497c9ba1e8cfddca8831c7da1
James Stidard <jamesstidard@James-MBP.home>
2015-08-03 20:04:43 +0100

    added other static methods for password functions

--- src/crypto.php
+++ src/crypto.php
@@ -8,9 +8,16 @@ include 'crypto/password_store.php';
 */
 class Crypto
 {
-	
 	public static function create_password($password, $salt=null) {
 		return create_password($password, $salt);
+	}
+
+	public static function validate_password($stored_password="", $guessed_password="", $update_stratagem=true, $legacy_password_validator=null) {
+		return validate_password($stored_password, $guessed_password, $update_stratagem, $legacy_password_validator);
+	}
+
+	public static function change_password($stored_password, $old_password, $new_password, $new_salt=null, $legacy_password_validator=null) {
+		return change_password($stored_password, $old_password, $new_password, $new_salt, $legacy_password_validator)
 	}
 }
 
